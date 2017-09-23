342e387dca7b077ab4bfe8d62226fbca07b37bf0
MattWillcox <mattjwillcox@gmail.com>
2017-09-23 12:50:03 -0700

    Adds main switch statement and starts NileInfo

--- nile_react/src/components/Main/Home/Home.jsx
+++ nile_react/src/components/Main/Home/Home.jsx
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>I am a Home.</div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '30em',
+        backgroundColor: '#d3d3d3',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+      }}>
+        <div>
+          <h1 style={{ color: 'white' }}>How It Works:</h1>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignContent: 'space-between', flex: 1}}>
+          <div style={{ height: '15em', width: '15em', backgroundColor: '#FEFEFE', borderRadius: '50%'}}>
+          </div>
+          <div style={{ height: '15em', width: '15em', backgroundColor: '#FEFEFE', borderRadius: '50%'}}>
+          </div>
+          <div style={{ height: '15em', width: '15em', backgroundColor: '#FEFEFE', borderRadius: '50%'}}>
+          </div>
+        </div>
+
+      </div>
     );
   }
 }
--- nile_react/src/components/Main/Main.jsx
+++ nile_react/src/components/Main/Main.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Home from './Home/Home.jsx';
 
 class Main extends Component {
+  state = {
+    currentUser: this.props.currentUser,
+    loggedIn: this.props.loggedIn,
+    userType: this.props.userType,
+    currentPage: 'Home'
+  }
+
+
+
   render() {
-    return (
-      <div>I am the main.</div>
-    );
+    const {currentUser, loggedIn, userType} = this.props;
+    switch(this.state.currentPage) {
+      case 'Home':
+        return (
+          <Home currentUser={currentUser} loggedIn={loggedIn} userType={userType} />
+        );
+        break;
+
+      // case 'Profile':
+      //   return (
+      //     <Profile />
+      //   );
+      //   break;
+    }
   }
 }
 
