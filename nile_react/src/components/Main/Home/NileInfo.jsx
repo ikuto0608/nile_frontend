6bab7c843dac03c867823a8ff90c65bf875f6c69
MattWillcox <mattjwillcox@gmail.com>
2017-09-23 13:17:33 -0700

    Completes Info Static Partial

--- nile_react/src/components/Main/Home/NileInfo.jsx
+++ nile_react/src/components/Main/Home/NileInfo.jsx
@@ -3,7 +3,43 @@ import React, { Component } from 'react';
 class NileInfo extends Component {
   render() {
     return (
-      <div>I am a NileInfo.</div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '30em',
+        backgroundColor: '#d3d3d3',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        color: 'white'
+      }}>
+        <div>
+          <h1>How It Works:</h1>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', width: '100%'}}>
+          <div style={{display: 'flex', flexDirection: 'column' }}>
+            <div style={{ height: '15em', width: '15em', backgroundColor: '#FEFEFE', borderRadius: '50%'}}>
+            </div>
+            <div style={{width: '15em'}}>
+              <p>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah </p>
+            </div>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column' }}>
+            <div style={{ height: '15em', width: '15em', backgroundColor: '#FEFEFE', borderRadius: '50%'}}>
+            </div>
+            <div style={{width: '15em'}}>
+              <p>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah </p>
+            </div>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column' }}>
+            <div style={{ height: '15em', width: '15em', backgroundColor: '#FEFEFE', borderRadius: '50%'}}>
+            </div>
+            <div style={{width: '15em'}}>
+              <p>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah </p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
