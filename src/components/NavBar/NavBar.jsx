25a9819efdc60736f6846b96edecc58b452a3763
wesley-wong <wes.wong@hotmail.com>
2017-09-24 01:22:38 -0700

    search filter styling, navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,8 +5,8 @@ class NavBar extends Component {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          
-        </div>
+        <a href="https://gyazo.com/1b7b0cbe9466b16bd83ed9a2d60da97c"><img src="https://i.gyazo.com/1b7b0cbe9466b16bd83ed9a2d60da97c.png" alt="https://gyazo.com/1b7b0cbe9466b16bd83ed9a2d60da97c" width="112" height="28"/></a>
+       </div>
       </nav>
     );
   }
