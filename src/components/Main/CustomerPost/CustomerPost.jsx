516c204c43e3eac7e89566f5e47a878b8448812e
wesley-wong <wes.wong@hotmail.com>
2017-09-24 09:34:53 -0700

    frontend things

--- src/components/Main/CustomerPost/CustomerPost.jsx
+++ src/components/Main/CustomerPost/CustomerPost.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Overlay from './Overlay.jsx';
+import Iframe from 'react-iframe'
+
 
 const testData = {
   userName: 'Matt Willcox',
@@ -29,6 +31,7 @@ class CustomerPost extends Component {
   render() {
     const visibility = this.state.visible ? 'visible' : 'hidden';
     const {userName, wantFrom, deliverTo, deliverBy, product, message, avatar} = testData;
+
     return (
       <div>
       <Overlay visibility={visibility} showOverlay={this.showOverlay} />
@@ -74,9 +77,13 @@ class CustomerPost extends Component {
             <p>Delivered From: GREEN</p>
             <p>Delivered To: RED</p>
           </div>
-          <div>
-            <div style={{height: '300px', width: '500px', backgroundColor: 'steelblue'}}>
-            </div>
+          <div>  
+            <Iframe url="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d52494819.207226716!2d152.40164407209093!3d36.57750710214443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e4!4m5!1s0x548673f143a94fb3%3A0xbb9196ea9b81f38b!2sVancouver%2C+British+Columbia!3m2!1d49.2827291!2d-123.12073749999999!4m5!1s0x605d1b87f02e57e7%3A0x2e01618b22571b89!2sTokyo%2C+Japan!3m2!1d35.6894875!2d139.6917064!5e0!3m2!1sen!2sca!4v1506231158803" 
+            width="600" 
+            height="450" 
+            frameborder="0" 
+            style="border:0" 
+            allowfullscreen/>
           </div>
         </div>
       </div>
